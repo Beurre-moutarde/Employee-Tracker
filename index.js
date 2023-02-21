9a2cc5caaf0e19445f63081fd8894cf730f46a79
Beurre-moutarde <christopher.m.lebreton@gmail.com>
2023-02-21 16:35:36 +0100

    added addemployee, addDepartment,add role, view and updateEmployee role function

--- index.js
+++ index.js
@@ -55,3 +55,249 @@ function start() {
             }
         })
 }
+
+function add() {
+    //set up prompts
+    inquirer.prompt([
+        //first question
+        {
+            name: "employeeRoleDepartment",
+            type: "list",
+            message: "What would you like to add?",
+            choices: ["Employee", "Role", "Department"]
+        }])
+        //questions for add
+        .then(function (response) {
+            switch (response.employeeRoleDepartment) {
+                case "Employee":
+                    addEmployee();
+                    break;
+                case "Role":
+                    addRole();
+                    break;
+                case "Department":
+                    addDepartment();
+                    break;
+                default: console.log("Please enter appropriate choice.")
+            }
+        })
+}
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            name: "first_name",
+            type: "input",
+            message: "What is the employee's first name?"
+        },
+
+        {
+            name: "last_name",
+            type: "input",
+            message: "What is the employee's last name?"
+        },
+        //link to role table for role id, this will provide department name
+        {
+            name: "role_id",
+            type: "input",
+            message: "What is the employee's role ID number?"
+        },
+
+        {
+            name: "manager_id",
+            type: "input",
+            message: "What is the manager's ID number?"
+        },
+
+        {
+            name: "id",
+            type: "number",
+            message: "What is the employee's id?"
+        },
+    ])
+        //update mysql table and display in console.table
+        .then(function (response) {
+            console.table(response)
+            let query = connection.query("INSERT INTO employee SET ?",
+                {
+                    first_name: response.first_name,
+                    last_name: response.last_name,
+                    role_id: response.role_id,
+                    manager_id: response.manager_id,
+                    id: response.id
+
+                },
+                function (err, res) {
+                    if (err) throw err
+                    console.log(res)
+                })
+            //values entered above from prompts
+            start()
+        })
+}
+
+function addDepartment() {
+    inquirer.prompt([
+
+        {
+            name: "ID_PK",
+            type: "number",
+            message: "What is the department ID?"
+        },
+
+        {
+            name: "name",
+            type: "input",
+            message: "What is the department name?"
+        },
+    ])
+
+        .then(function (response) {
+            console.table(response)
+            let query = connection.query("INSERT INTO department SET ?",
+                {
+                    ID_PK: response.ID_PK,
+                    name: response.name,
+
+                },
+                function (err, res) {
+                    if (err) throw err
+                    console.log(res)
+                })
+
+            start()
+        })
+}
+//end add department
+
+function addRole() {
+    inquirer.prompt([
+        {
+            name: "ID_PK",
+            type: "number",
+            message: "What is the role ID?"
+        },
+
+        {
+            name: "title",
+            type: "input",
+            message: "What is the role title?"
+        },
+
+        {
+            name: "salary",
+            type: "number",
+            message: "What is the role salary?"
+        },
+
+        {
+            name: "department_id",
+            type: "number",
+            message: "What is the department ID number?"
+        }
+    ])
+        .then(function (response) {
+            console.table(response)
+            //enter mysql code here
+            let query = connection.query("INSERT INTO role SET ?",
+                {
+                    ID_PK: response.ID_PK,
+                    title: response.title,
+                    salary: response.salary,
+                    department_id: response.department_id
+                },
+                function (err, res) {
+                    if (err) throw err
+                    console.log(res)
+                })
+
+            start()
+        })
+}
+function view() {
+    inquirer.prompt([
+        {
+            name: "choiceView",
+            type: "list",
+            message: "What would you like to view?",
+            choices: ["All Employees", "All Departments", "All Roles"]
+        }]).then(function (response) {
+            switch (response.choiceView) {
+
+                case "All Employees":
+                    employee();
+                    break;
+                case "All Departments":
+                    department();
+                    break;
+                case "All Roles":
+                    role();
+                    break;
+                default: console.log("Please enter appropriate choice.")
+
+            }
+        })
+}
+//attempting to join employee and role tables
+
+function employee() {
+    let query = "SELECT * FROM employee"
+
+    connection.query(query, function (err, res) {
+        console.table(res)
+        start()
+    })
+}
+
+function department() {
+    let query = "SELECT * FROM department"
+    connection.query(query, function (err, res) {
+        console.table(res)
+        start()
+    })
+}
+
+function role() {
+    let query = "SELECT * FROM role"
+    connection.query(query, function (err, res) {
+        console.table(res)
+        start()
+    })
+}
+
+function updateEmployeeRole() {
+    connection.query("SELECT id, first_name, last_name from employee", (err, employeeRes) => {
+        if (err) throw err;
+        // transform the data to be used in inquirer
+        const organizedEmployeeData = employeeRes.map(({ id, first_name, last_name }) => `${id}: ${first_name} ${last_name}`)
+        connection.query("SELECT ID_PK, title from role", (err, roleRes) => {
+            if (err) throw err;
+            const organizedRoleData = roleRes.map(({ ID_PK, title }) => `${ID_PK}: ${title}`);
+            // evidence collected above
+            inquirer.prompt([
+                {
+                    type: "list",
+                    choices: organizedEmployeeData,
+                    name: "targetEmp",
+                    message: "Whose role needs to change?"
+                }, {
+                    type: "list",
+                    choices: organizedRoleData,
+                    name: "targetRole",
+                    message: "What is their new role?"
+                }
+            ]).then(({ targetEmp, targetRole }) => {
+                let id = targetEmp.split(":")[0]; 
+                let role_ID = targetRole.split(":")[0];
+                connection.query("UPDATE employee SET role_ID = ? WHERE id = ?", [role_ID, id], (err, success) => {
+                    if (err) throw err;
+                    console.log(`Your changes were successful`);
+                    process.exit();
+                })
+            })
+
+        })
+    })
+
+}
+
